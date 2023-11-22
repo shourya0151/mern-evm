848fd84ed3d4b619b548a2802c627257cd80b7af
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-22 19:55:44 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from '../api/routes/user.route.js'
 import authRouter from '../api/routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from '../api/routes/listing.route.js';
+import path from 'path';
+
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() =>{
     }).catch((err)=>{
         console.log(err);
     })
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,6 +30,11 @@ app.listen(3000, () => {
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
